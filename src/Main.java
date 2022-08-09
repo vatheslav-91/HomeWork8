72333cb3dbad29cd1fe66fc941bf45f5d807407d
Вячеслав <vatheslav-91@mail.ru>
2022-08-10 00:28:48 +0500

    ДЗ Строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String lastName = "Ivanov";
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String fullName = lastName + "  " + firstName + "  " + middleName;
+        System.out.println("ФИО сотрудника " + fullName);
+
+        System.out.println("Данные ФИО сотрудника для заполнения  отчета - " + fullName.toUpperCase());
+
+
+        fullName = "Иванов Семён Семёнович";
+
+        String replaceFullName = fullName.replace("ё", "е");
+
+        System.out.println("Данные ФИО сотрудника - " + replaceFullName);
+
     }
 }
